f9dbd6f74737fe43ee8386de887c072de8043b6a
p.r <mytimenow@gmail.com>
2020-04-12 01:59:45 -0400

    added to test for admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,14 +5,14 @@ from django.urls import reverse
 
 class AdminSiteTests(TestCase):
     """Used for testing the admin pages"""
-    def setup(self):
+    def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email = 'starboy@gmail.com',
             password = 'password'
         )
 
-        self.client.force_login(self.admim_user)
+        self.client.force_login(self.admin_user)
 
         self.user = get_user_model().objects.create_user(
             email = 'test@tes.com',
@@ -20,9 +20,16 @@ class AdminSiteTests(TestCase):
             name ='Test user'
         )
 
-     def test_urls_listed(self):
-         """the uses are listed on a page"""
-         url = reverse('admin:core_user_changelist')
+    def test_urls_listed(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-         self.assertContains(res, self.user.name)
-         self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works in admin"""
+        url =  revers('admin:core_user_change')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
